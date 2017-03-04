421517cb6391044e2ca5c6110419dd7c43946d4b
Gabriel <Gabriel@LAPTOP-EI97OEJ9>
2017-03-03 20:53:16 -0500

    Add project files.

---
+++ Project2/Project1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Project2
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Project1
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new ModelRenderer())
+                game.Run();
+        }
+    }
+#endif
+}
